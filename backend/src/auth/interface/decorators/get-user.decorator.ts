933ea499cb8919327072a7766c11c09985941fe0
naninaniawiks <erlandagsya2@gmail.com>
2025-11-17 01:27:21 +0700

    test(get-user.decorator)

--- backend/src/auth/interface/decorators/get-user.decorator.ts
+++ backend/src/auth/interface/decorators/get-user.decorator.ts
@@ -1,5 +1,10 @@
+// backend\src\auth\interface\decorators\get-user.decorator.ts
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * Custom decorator (@GetUser) untuk mengekstrak objek 'user'
+ * yang telah ditambahkan ke request oleh 'AuthGuard'.
+ */
 export const GetUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
